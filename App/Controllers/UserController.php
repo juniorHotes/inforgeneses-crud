c740ce79ca41dd0765eda8006aeb977bba40c593
juniorHotes <juniorhotes53@gmail.com>
2023-03-02 13:52:54 -0300

    Feat: UserController

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -1,8 +1,25 @@
 <?php
+namespace App\Controllers;
 
-class UserController {
+class UserController extends Controller {
 
-    public function index(array $params) {
-        
+    public function render_create(array $params) {
+        return $this->view('user-create');
+    }
+
+    public function render_edit(array $params) {
+        return $this->view('user-edit');
+    }
+
+    public function create(array $params) {
+        var_dump($params);
+    }
+
+    public function edit(array $params) {
+        var_dump($params);
+    }
+
+    public function delete(array $params) {
+        var_dump($params);
     }
 }
