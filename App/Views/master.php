a16add36865b6a286e5eda4b26743eb5f48f89d3
juniorHotes <juniorhotes53@gmail.com>
2023-03-02 13:54:06 -0300

    Style: Margem no container

--- App/Views/master.php
+++ App/Views/master.php
@@ -10,7 +10,7 @@
     <title>Inforgenenses CRUD</title>
 </head>
 <body>
-    <div class="container-sm mt-5">
+    <div class="container-sm mt-5 mb-5">
         <?= $this->section("content") ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
