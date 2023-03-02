c20064740dac1c04590258466bafba18c180a160
juniorHotes <juniorhotes53@gmail.com>
2023-03-02 13:52:39 -0300

    Feat: LoginController

--- App/Controllers/LoginController.php
+++ App/Controllers/LoginController.php
@@ -1,8 +1,16 @@
 <?php
+namespace App\Controllers;
 
-class LoginController {
+class LoginController extends Controller {
 
     public function index(array $params) {
+        return $this->view('login');
+    }
 
+    public function login(array $params) {
+
+        var_dump($params);
+
+        // header('location: /');
     }
 }
