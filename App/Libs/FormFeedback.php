dad462e0563e0a78d6ea1d48700fc17e63f796b2
juniorHotes <juniorhotes53@gmail.com>
2023-03-03 16:20:58 -0300

    Feat: Feedback do formulário

---
+++ App/Libs/FormFeedback.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Libs;
+
+class FormFeedback {
+
+    private $data;
+
+    function __construct(string $data) {
+        if($_SERVER['REQUEST_METHOD'] = "POST") {
+            $this->data = json_decode(str_replace('&quot;', '"', $data));
+        }
+    }
+
+    public function form_feedback() {
+        return isset($this->data->message) ? $this->data->message : '';
+    }
+    
+    public function field_feedback(string $field_id) {
+        return isset($this->data->fields->$field_id) ? $this->data->fields->$field_id->message : '';
+    }
+
+    public function field_val(string $field_id) {
+        return isset($this->data->params) ? $this->data->params->$field_id : '';
+    }
+
+    public function is_invalid(string $field_id) {
+        return isset($this->data->fields->$field_id) ? 'is-invalid' : '';
+    }
+}
