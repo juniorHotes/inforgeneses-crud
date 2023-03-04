5250fb0a73054d36dab8d1dfcdcdf45ba01469b1
juniorHotes <juniorhotes53@gmail.com>
2023-03-03 23:31:13 -0300

    Feat: Obter todos os usuários.

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -1,9 +1,15 @@
 <?php
 namespace App\Controllers;
 
+use App\Models\UserModel;
+
 class HomeController extends Controller {
 
     public function index(array $params) {
-       return $this->view('home');
+       $user_model = new UserModel();
+
+       $users = $user_model->get_all();
+
+       return $this->view('home', [ 'users' => json_encode($users) ]);
     }
 }
