1a78862707f7100d883014d111076a3082aa8e21
juniorHotes <juniorhotes53@gmail.com>
2023-03-04 17:30:55 -0300

    Feat: login de usuário.

--- App/Controllers/LoginController.php
+++ App/Controllers/LoginController.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Controllers;
 
+use App\Libs\FormValidation;
+use App\Models\UserModel;
+
 class LoginController extends Controller {
 
     public function index(array $params) {
@@ -9,8 +12,104 @@ class LoginController extends Controller {
 
     public function login(array $params) {
 
-        var_dump($params);
+        $login = htmlspecialchars($params['login']);
+        
+        $get_by = 'name';
 
-        // header('location: /');
+        $success = true;
+        $fields = [];
+        
+        if(FormValidation::is_email($login)) {
+            $get_by = 'email';
+        }
+        
+        $user_model = new UserModel();
+        $user = $user_model->get_user_by($get_by, $login);
+
+        if($get_by === 'name' && !FormValidation::user_exists($login)) {
+            $success = false;
+
+            $fields['login'] = [
+                'value' => $params['login'],
+                'message' => 'Este usuário não está cadastrado'
+            ];
+
+            return $this->view('login', 
+                [ 
+                    'data' => json_encode([
+                        'params' => $params,
+                        'message' => '',
+                        'fields' => $fields,
+                        'success' => $success
+                    ])
+                ]);
+        }
+
+        if($get_by === 'email' && !FormValidation::email_exists($login)) {
+            $success = false;
+
+            $fields['login'] = [
+                'value' => $params['login'],
+                'message' => 'Este e-mail não está cadastrado.'
+            ];
+
+            return $this->view('login', 
+                [ 
+                    'data' => json_encode([
+                        'params' => $params,
+                        'message' => 'Verifique se as senhas são iguais',
+                        'fields' => $fields,
+                        'success' => $success
+                    ])
+                ]);
+        }
+
+        if(!FormValidation::password_verify($user['id'], $params['pass'])) {
+            $success = false;
+
+            $fields['pass'] = [
+                'value' => $params['pass'],
+                'message' => 'Sua senha está incorreta.'
+            ];
+
+            return $this->view('login', 
+                [ 
+                    'data' => json_encode([
+                        'params' => $params,
+                        'message' => '',
+                        'fields' => $fields,
+                        'success' => $success
+                    ])
+                ]);
+        }
+
+        if($success) {
+                        
+            session_start();
+            $_SESSION['user'] = $user['id'];
+
+            header('location: /user/edit?user=' . $user['id']);
+        }
+    }
+
+    /**
+     * Retorna o id do usuário logado, se não está logado o redireciona para /login.
+     * @return int Retorna o id do usuário logado
+     */
+    public static function current_user_id() {
+        
+        if(!isset($_SESSION['user'])) {
+            header('location: /login');
+        } 
+        return $_SESSION['user'];
+    }
+
+    /**
+     * Verifica se o usuário está logado.
+     * @return boolean true|false
+     */
+    public static function is_logged() {
+        session_start();
+        return isset($_SESSION['user']);
     }
 }
--- App/Views/login.php
+++ App/Views/login.php
@@ -1,18 +1,20 @@
 <?php $this->layout("master") ?>
 
+<?php $form = new \App\Libs\FormFeedback($this->e(isset($data) ? $data : '')) ?>
+
 <div class="container col-xxl-5 col-lg-5 col-md-6 border py-4 px-5 rounded-3">
     <h3>Entre com seu usuário</h3>
     <hr>
     <form class="pt-2 needs-validation" method="POST" action="/login">
       <div class="mb-3">
-        <label for="user_login" class="form-label">E-mail ou usuário</label>
-        <input type="text" class="form-control required" name="user_login" id="user_login">
-        <div class="invalid-feedback" id="user_login-feedback"></div>
+        <label for="login" class="form-label">E-mail ou usuário</label>
+        <input type="text" class="form-control required <?= $form->is_invalid('login') ?>" name="login" id="login" value="<?= $form->field_val('login') ?>">
+        <div class="invalid-feedback" id="login-feedback"><?= $form->field_feedback('login') ?></div>
       </div>
       <div class="mb-3">
         <label for="pass" class="form-label">Senha</label>
-        <input type="password" class="form-control required" name="pass" id="pass">
-        <div class="invalid-feedback" id="pass-feedback"></div>
+        <input type="password" class="form-control required <?= $form->is_invalid('pass') ?>" name="pass" id="pass" value="<?= $form->field_val('pass') ?>">
+        <div class="invalid-feedback" id="pass-feedback"><?= $form->field_feedback('pass') ?></div>
       </div>
       <hr>
       <div class="d-grid gap-2">
