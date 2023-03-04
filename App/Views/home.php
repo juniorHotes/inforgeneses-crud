401b55cabd6cfc3bb16af127ade796b9744ee1ee
juniorHotes <juniorhotes53@gmail.com>
2023-03-04 19:19:59 -0300

    Feat: Responssividade da tabela e formatação de data

--- App/Views/home.php
+++ App/Views/home.php
@@ -4,8 +4,8 @@
     $users = json_decode(str_replace('&quot;', '"', $this->e($users)));
 ?>
 
-<div class="container border pt-4 px-5 rounded-3 ">
-    <a href="/user/create" class="btn btn-primary">Adicionar</a>
+<div class="container border pt-4 px-5 rounded-3 table-responsive">
+    <a href="/user/create" class="btn btn-primary"><i class="bi bi-person-plus-fill"></i> Adicionar novo usuário</a>
     <table class="table caption-top">
     <caption>Lista de usuários</caption>
     <thead>
@@ -22,7 +22,15 @@
             <th scope="row"><a href="/user/edit?user=<?= $user->id ?>" class="link-primary"><?= $user->id ?></a></th>
             <td><a href="/user/edit?user=<?= $user->id ?>" class="link-primary"><?= $user->name ?></a></td>
             <td><a href="/user/edit?user=<?= $user->id ?>" class="link-primary"><?= $user->email ?></a></td>
-            <td><a href="/user/edit?user=<?= $user->id ?>" class="link-primary"><?= $user->created_at ?></a></td>
+            <td>
+                <a href="/user/edit?user=<?= $user->id ?>" class="link-primary">
+                <?php 
+                    $date = new DateTime($user->created_at);
+                    $date->setTimezone(new DateTimeZone('America/Sao_Paulo'));
+                    echo $date->format("d/m/Y H:i:s"); 
+                ?>
+                </a>
+            </td>
         </tr>
         <?php endforeach; ?>
     </tbody>
