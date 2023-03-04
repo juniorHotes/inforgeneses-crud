5736d924ed8500fddd1d886a62463232cb8f233a
juniorHotes <juniorhotes53@gmail.com>
2023-03-04 20:25:58 -0300

    Feat: Removido a crição de tabela.

--- App/Config/Database.php
+++ App/Config/Database.php
@@ -14,25 +14,9 @@ class Database {
         try {
             $this->conn = new \PDO("mysql:host=" . $this->hostname . ";dbname=" . $this->dbname, $this->username, $this->userpass);
             $this->conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-            $this->create_data_base();
         } catch (\PDOException $exc) {
             echo 'PDO Error: ' . $exc->getMessage();
         }
-    }
-
-    private function create_data_base() {
-
-        $query = "CREATE TABLE IF NOT EXISTS `inforgenenses_crud`.`users` (
-                    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                    name VARCHAR(255) NOT NULL,
-                    email VARCHAR(255) NOT NULL,
-                    password VARCHAR(16) NOT NULL,
-                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
-                )";
-
-        $prepare = $this->conn->prepare($query);
-
-        $prepare->execute();
     }
 
     public function query($sql) {
