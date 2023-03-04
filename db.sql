abb7f4f6d51faee5d273d348085bb2ec32ee3ffe
juniorHotes <juniorhotes53@gmail.com>
2023-03-04 20:26:54 -0300

    Feat: :tada: Criar db e tabela

---
+++ db.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA `inforgenenses_crudw` ;
+
+CREATE TABLE IF NOT EXISTS `inforgenenses_crudw`.`users` (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(16) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+)
